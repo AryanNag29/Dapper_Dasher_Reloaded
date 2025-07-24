8ca17823b3202f80e2efab806a8c995af02e4d6d
AryanNag29 <231030332@juitsolan.in>
2025-07-25 00:03:04 +0530

    random spawn distance at X axis for nebula

--- Assets/Scripts/Pipe_spawner_Script.cs
+++ Assets/Scripts/Pipe_spawner_Script.cs
@@ -6,16 +6,34 @@ public class Pipe_spawner_Script : MonoBehaviour
 {
     //SerializeField
     [SerializeField] private GameObject nebula;
+
+    //variables
+
+    //spawnrate
     public float spawnRate = 2f;
     public float timer = 0.0f;
 
+    //spawn at reandom distance at X
+    public float spawnRangeX1 = 2f;
+    public float spawnRangeX2 = 11f;
+    public float spawnRangeY = -0.03f;
+    public float spawnRangeZ = 0f;
 
 
-    //function
+
+
+    //functions
+
+
+    //spawnPipe at random X
     private void spawnPipe()
     {
-        Instantiate(nebula, transform.position, transform.rotation);
+        float randomX = UnityEngine.Random.Range(spawnRangeX1, spawnRangeX2); //Always use unityengine before using random 
+        Vector3 SpawnPosition = new Vector3(randomX, spawnRangeY, spawnRangeZ);
+        Instantiate(nebula, SpawnPosition, transform.rotation);
     }
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,6 +44,7 @@ public class Pipe_spawner_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
+        
         //spawn rate for the time
         if (timer < spawnRate)
         {
