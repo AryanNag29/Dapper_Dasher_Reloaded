81eb626f8604530f7978d943fc23f4037c0f121c
AryanNag29 <231030332@juitsolan.in>
2025-07-24 23:16:03 +0530

    nebula gameObjects will get destroyed after x distance -12.1

--- Assets/Scripts/Nebula_Script.cs
+++ Assets/Scripts/Nebula_Script.cs
@@ -4,6 +4,7 @@ public class Nebula_Script : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D nebulaBoxCollider;
     public int nebulaMovement = 5;
+    private float deadZone = -12.1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,11 @@ public class Nebula_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position+ Vector3.left*nebulaMovement*Time.deltaTime;
+        transform.position = transform.position + Vector3.left * nebulaMovement * Time.deltaTime;
+
+        if (transform.position.x < deadZone)
+        {
+            Destroy(gameObject);
+        }
     }
 }
