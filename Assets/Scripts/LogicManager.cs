8ccbb1b9548d09419f6eb4f11c58d4eee456d76b
AryanNag29 <231030332@juitsolan.in>
2025-08-05 23:40:45 +0530

    Adding score board for the player

---
+++ Assets/Scripts/LogicManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogicManager : MonoBehaviour
+{
+    public int playerScore;
+    [SerializeField] private Text scoreText; //reference to the text under ui
+
+    //functions
+    [ContextMenu("Increase Score")] //add another context menu to run anything below it like the addScore function
+    //addScore to he UI
+    public void addScore()
+    {
+        playerScore += 1;
+        scoreText.text = playerScore.ToString();
+    }
+}
