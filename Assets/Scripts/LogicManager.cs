6e44f4fd6d274f4f34b10b2a2079460129ddc2b7
AryanNag29 <231030332@juitsolan.in>
2025-08-06 23:49:13 +0530

    Also adding a Button to restart the level

--- Assets/Scripts/LogicManager.cs
+++ Assets/Scripts/LogicManager.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicManager : MonoBehaviour
 {
     public int playerScore;
-    public bool isGameOver;
+    public bool isGameOver = false;
+    
     [SerializeField] private Text scoreText; //reference to the text under ui
     [SerializeField] private Text GameOverText;
 
@@ -17,6 +19,17 @@ public class LogicManager : MonoBehaviour
         playerScore += scoreToAdd;
         scoreText.text = playerScore.ToString();
     }
-
+    [ContextMenu("GameOver")]
+    public void displayGameOver()
+    {
+        if (isGameOver)
+        {
+            GameOverText.text = "GameOver";
+        }
+        else
+        {
+            GameOverText.text = "";
+        }
+    }
     
 }
