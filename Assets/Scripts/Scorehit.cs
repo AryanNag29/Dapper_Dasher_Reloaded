a9c02559bbd8121023821e3b40897d1353ff252d
AryanNag29 <231030332@juitsolan.in>
2025-08-06 08:31:51 +0530

    implemented score system for the player

---
+++ Assets/Scripts/Scorehit.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Scorehit : MonoBehaviour
+{
+    public LogicManager logic;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        logic = GameObject.FindWithTag("Logic").GetComponent<LogicManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        logic.addScore();
+    }
+}
