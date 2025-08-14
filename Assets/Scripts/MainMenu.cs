b28ae05f2d4a254cfc47421311280bd21169ebc5
AryanNag29 <231030332@juitsolan.in>
2025-08-14 23:54:40 +0530

    Added audio to main menu too

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public audioManager audio;
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
@@ -10,5 +11,11 @@ public class MainMenu : MonoBehaviour
     public void quit()
     {
         Application.Quit();
+    }
+    void Start()
+    {
+        audio = GameObject.FindGameObjectWithTag("Audio").GetComponent<audioManager>();
+        audio.musicSource.clip = audio.mainMenu;
+        audio.musicSource.Play();
     }
 }
